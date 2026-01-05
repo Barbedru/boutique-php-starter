dc306273344e393bf72bf699f45763b83a7da432
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-05 15:54:53 +0100

    Le piège des types

---
+++ exercices/jour-01/types.php
@@ -0,0 +1,13 @@
+<?php
+$a = "5";
+$b = 3;
+$c = $a + $b;
+
+var_dump($a);      // affiche string car 5 est entre "" c'est donc une chaîne de caractère
+var_dump($b);      // affiche int 
+var_dump($c);      // un string additionné à un int donne un int
+
+$price = "29.99€";        // c'est un string au lieu d'un float et il y a le symbole euro
+$result = $price + 10;
+var_dump($result);       // celà affiche float(39.99) car PHP ignore le symbole euro et convertit le string en float pour faire l'addition
+?>
