6be2021c515e9c931d4fe9e934424a177cb004cb
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-05 16:40:41 +0100

    PHP dans HTML

---
+++ exercices/jour-01/fiche.php
@@ -0,0 +1,19 @@
+<?php
+$name = "Chaussettes";
+$price = 15.99;
+$stock = 250;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chaussettes</title>
+</head>
+<body>
+    <h1><?= $name ?></h1>
+    <p>Prix : <?= $price ?> €</p>
+    <p>Stock : <?= $stock ?></p>
+</body>
+</html>
