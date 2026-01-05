7087b9d484520a973dd757511b235ccfd0019df3
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-05 15:03:57 +0100

    Variable produit

---
+++ exercices/jour-01/produit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$name = "Chaussettes";
+$price = 15.99;
+$stock = 250;
+$onSale = true;
+var_dump($name, $price, $stock, $onSale);
+echo "Le produit $name coûte $price euros.";
+
+?>
+
+</body>
+</html>
