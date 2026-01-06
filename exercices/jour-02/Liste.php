d4cb929cce54fe710410cd126e66e2874d8d00cb
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-06 10:37:01 +0100

    Liste de courses

---
+++ exercices/jour-02/Liste.php
@@ -0,0 +1,11 @@
+<?php
+$groceries = array("Apples", "Bananas", "Carrots", "Kiwis", "Lemons");
+
+var_dump($groceries);                                     // Affiche la structure complète du tableau
+
+echo $groceries[1];                                      //Bananas
+
+echo count($groceries);                                 // Affiche le nombre d'articles => 5
+                                                   
+
+?>
