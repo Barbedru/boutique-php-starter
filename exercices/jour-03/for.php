401d2b763169d06b3aa1e315002724350db5ae89
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-07 11:52:04 +0100

    Boucle for

---
+++ exercices/jour-03/for.php
@@ -0,0 +1,33 @@
+<?php
+
+for ($i = 0; $i < 11; $i++) {
+    if ($i % 2 == 0)
+     echo "Num paire : " . $i . "<br>";     // affiche les nombres pairs
+    else
+     echo "Num impaire : " . $i . "<br>";   // affiche les nombres impaires
+}
+
+?>
+
+<br>
+
+<?php
+
+for ($i = 10; $i > -1 ; $i--) {
+    
+   echo $i . "<br>";
+
+}
+
+?>
+
+<br>
+
+<?php
+
+for ($i = 0; $i < 11 ; $i++) {
+    echo ("Table de 7 : " . $i * 7 . "<br>" );
+   
+}
+
+?>
