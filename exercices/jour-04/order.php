162ee5c8f86d347e674cb2ea55e950450b2a45d2
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-08 11:55:31 +0100

    Statut de commande

---
+++ exercices/jour-04/order.php
@@ -0,0 +1,64 @@
+// SWITCH
+
+<?php
+$status = "stanby" . "validated" . "shipped" . "delivered" . "canceled";
+
+switch ($status){
+     case "standby";
+    break;
+     case "validated";
+    break;
+     case "shipped";
+    break;
+     case "delivered";
+    break;
+     case "canceld";
+    break;
+}
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<p><span style="color: orange"><?= $status?></span></p>
+
+</body>
+</html>
+
+
+// MATCH
+
+<?php
+$status = "validated";
+
+$return_status = match ($status){
+        "standby" => 'En attente',
+        "validated" => 'Validé',
+        "shipped" => 'Envoyé',
+        "delivered" => 'Livré',
+        "canceled" => 'Annulé',
+
+};
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<p><span style="color: green"><?= $return_status?></span></p>
+
+</body>
+</html>
