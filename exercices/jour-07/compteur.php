d1f684f757cdf28ce5ec9cf29d09e41e602376e5
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-14 15:29:03 +0100

    Session basique : Compteur de visites

---
+++ exercices/jour-07/compteur.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();                      //Toujours en première ligne
+
+
+
+if (!isset($_SESSION["visits"])) {
+    $_SESSION["visits"] = 1;
+} else {
+    $_SESSION["visits"]++;
+}
+
+if (isset($_GET["reset"])) {
+    $_SESSION["visits"] = 0;
+}
+
+
+$visit = $_SESSION["visits"];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1> Vous avez visité cette page <?= $visit ?> fois.</h1>
+    <button> <a href="compteur.php"> Compteur </a></button>
+    <button> <a href="compteur.php?reset=0"> Réinitialiser </></button>
+
+</body>
+
+</html>
