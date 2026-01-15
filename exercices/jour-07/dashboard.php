bf5e341623592f3c88a02125226d6e892cb20d2b
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-15 11:12:22 +0100

    Connexion utilisateur simulée

---
+++ exercices/jour-07/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION["user"])) {
+    header("Location: login.php");
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+ 
+<p> Bienvenue <?= $_SESSION["user"] ?></p >
+
+<button> <a href="logout.php"> Déconnexion </a></button>
+
+</body>
+
+</html>
