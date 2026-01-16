f356ee521496dce1e958b1226492d4095e894951
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-01-16 16:53:03 +0100

    Première classe

--- exercices/jour-08/Car.php
+++ exercices/jour-08/Car.php
@@ -4,9 +4,13 @@
 //Créer une classe
 class Car{
     // Propriétés (les données)
-    public string $brand;
-    public string $model;
-    public int $year;
+
+    public function __construct(
+    
+    public string $brand,
+    public string $model,
+    public int $year,
+    ){}
     
     // Méthode (le comportement)
     public function display(): void
@@ -24,13 +28,19 @@ class Car{
 //On peut créer plusieurs objets (instances) d'une même classe.
 //L'objet est créé avec le mot-clé new et est stocké dans une variable.
 //Ses propriétés et méthodes sont accessibles avec -> et peuvent être modifiées indépendamment.
-$Car = new Car();
-$Car->brand = "Voiture";
-$Car->model = "Qui roule";
-$Car->year = 2007;
+$car = new Car("Voiture", "Qui roule", 2007);
+$car1 = new Car("Voiture", "Qui roule vite", 2005);
+$car2 = new Car("Voiture", "Qui roule très vite", 2024);
 
-$Car->getAge();
+$car->getAge();
 echo"<br>";
-$Car->display();
+$car->display();
+echo"<br>";
+
+$car1->display();
+
+echo"<br>";
+$car2->display();
+echo"<br>";
 
 ?>
